9ce257d067166ff58344f243daff42bfa7b5d12d
GERB-DEVELOPER <gerb@streamviu.com>
2026-03-29 18:00:08 -0500

    feat: redesign inicio with hero section and category cards

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
-
-  public titulo:string;
-  public pulsaciones: number;
-
-  constructor(){
-    this.titulo="Universidad Internacional de Valencia y Paris";
-    this.pulsaciones=0;
-   
-  }
-  pulsar():void{
-    this.pulsaciones++;
-  }
-  resetPulsaciones():void{
-    this.pulsaciones=0;
-  }
-}
+export class InicioComponent {}
